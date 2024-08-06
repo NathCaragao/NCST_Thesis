cc6aa88025dc3ffc4b17380c7208efd550f0f110
NathCaragao <nathanielcaragao@gmail.com>
2024-08-06 19:06:41 +0800

    Initial decoupling of Server and Authentication

--- src/ServerManager/Authentication/Authentication.gd
+++ src/ServerManager/Authentication/Authentication.gd
@@ -1,11 +1,29 @@
 extends Node
 
+@export var serverManager : Node = null
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+#
+#func handleRequestEmailAndPassLogin(email: String, password: String):
+	#_session = await _client.authenticate_email_async(email, password, null, false)
+	#if not _session.is_exception():
+		#return OK
+	#else: 
+		#return null
+	#
+#func handleRequestEmailAndPassRegister(username: String, email: String, password: String):
+	#_session = await _client.authenticate_email_async(email, password, null, true)
+	#if not _session.is_exception():
+		## Change the displayname of user
+		#var update : NakamaAsyncResult = await _client.update_account_async(_session, null,username, null, null, null)
+		#return OK
+	#else:
+		#return null
+	#
+#
+#func loginWithGoogle():
+	#pass
+#
+#func logoutUser():
+	#await Server._client.session_logout_async(Server._session)
+	#_session = null
+	#_socket = null
--- src/ServerManager/ServerManager.gd
+++ src/ServerManager/ServerManager.gd
@@ -7,30 +7,30 @@ const SERVER_KEY := "thesisServer"
 @onready var _socket : NakamaSocket = null
 
 
-func handleRequestEmailAndPassLogin(email: String, password: String):
-	_session = await _client.authenticate_email_async(email, password, null, false)
-	if not _session.is_exception():
-		return OK
-	else: 
-		return null
-	
-func handleRequestEmailAndPassRegister(username: String, email: String, password: String):
-	_session = await _client.authenticate_email_async(email, password, null, true)
-	if not _session.is_exception():
-		# Change the displayname of user
-		var update : NakamaAsyncResult = await _client.update_account_async(_session, null,username, null, null, null)
-		return OK
-	else:
-		return null
-	
-
-func loginWithGoogle():
-	pass
-
-func logoutUser():
-	await Server._client.session_logout_async(Server._session)
-	_session = null
-	_socket = null
+#func handleRequestEmailAndPassLogin(email: String, password: String):
+	#_session = await _client.authenticate_email_async(email, password, null, false)
+	#if not _session.is_exception():
+		#return OK
+	#else: 
+		#return null
+	#
+#func handleRequestEmailAndPassRegister(username: String, email: String, password: String):
+	#_session = await _client.authenticate_email_async(email, password, null, true)
+	#if not _session.is_exception():
+		## Change the displayname of user
+		#var update : NakamaAsyncResult = await _client.update_account_async(_session, null,username, null, null, null)
+		#return OK
+	#else:
+		#return null
+	#
+#
+#func loginWithGoogle():
+	#pass
+#
+#func logoutUser():
+	#await Server._client.session_logout_async(Server._session)
+	#_session = null
+	#_socket = null
 
 func getUser():
 	return await _client.get_account_async(_session)
