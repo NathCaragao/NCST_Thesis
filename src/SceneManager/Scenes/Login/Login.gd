b1621754f2e64fb76d7aebc97bc8774bf0530517
NathCaragao <nathanielcaragao@gmail.com>
2024-08-12 09:31:06 +0800

    Initial transfer of Login screen under SceneManager dir

---
+++ src/SceneManager/Scenes/Login/Login.gd
@@ -0,0 +1,32 @@
+extends Control
+
+func _ready():
+	%GoogleBtn.disabled = true
+	%FbBtn.disabled = true
+
+	
+func resetLoginWindow():
+	%EmailInput.clear()
+	%PassInput.clear()
+
+func _on_signup_btn_pressed():
+	resetLoginWindow()
+	ScreenTransitions.load_scene("res://scenes/signupScreen/signup_window.tscn")
+
+func _on_login_btn_pressed():
+	# Change with direct calling
+	var loginResult = await Server.handleRequestEmailAndPassLogin(%EmailInput.text, %PassInput.text)
+	if loginResult == OK:
+		handleAuthSuccess()
+	else:
+		Notification.showMessage("Something went wrong, please try logging in again.", 5.0)
+
+func handleAuthSuccess():
+	resetLoginWindow()
+	Notification.showMessage("Login Successful!", 2.0)
+	await get_tree().create_timer(2.0).timeout
+	ScreenTransitions.load_scene("res://scenes/titleScreen/title_screen.tscn")
+
+
+func _on_quit_btn_pressed():
+	get_tree().quit()
