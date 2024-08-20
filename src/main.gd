4bd136209011cfaa3c73b0e6062afc7393a6d1d8
NathCaragao <nathanielcaragao@gmail.com>
2024-08-20 11:48:05 +0800

    Made the managers as autoload
    also made main.tscn queue_free() once it is loaded. Di na need ng main scene pero need padin ng entry point

--- src/main.gd
+++ src/main.gd
@@ -1,35 +1,4 @@
 extends Node
 
-@onready var ServerManager = $ServerManager
-@onready var MusicManager = $MusicManager
-@onready var SceneManager = $SceneManager
-
-
 func _ready():
-	connectToSignals()
-
-func connectToSignals():
-	# ServerManager related signals
-	#	- Auth
-	SignalsAutoload.requestEmailAndPassLogin.connect(handleRequestEmailAndPassLogin)
-	# SceneManager related signals
-	#SignalsAutoload.requestChangeScene.connect()
-
-# ServerManager related handlers
-#	- Auth
-func handleRequestEmailAndPassLogin(email : String, password : String):
-	# Steps to do:
-	# 1. Setup a loading screen
-	# -------------
-	# 2. Let ServerManager execute the login
-	var result = await ServerManager.loginWithEmailAndPassword(email, password)
-	# 3. Get the result of the login operation
-	if result == OK:
-		# Show success notification then change screeen to lobby
-		pass
-	else:
-		# Show failed notification and retain login screen
-		pass
-	# 4. Take down loading screen
-	# ---------------
-	
+	queue_free()
