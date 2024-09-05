c1255dc2307eaa7f7e2eab28163b0d0c37d59a66
chrono-04 <danielpenaflor04@gmail.com>
2024-09-06 00:07:26 +0800

    updated title screen gd

--- scenes/ui-scenes/title-screen/title_screen.gd
+++ scenes/ui-scenes/title-screen/title_screen.gd
@@ -6,3 +6,7 @@ func _on_start_btn_pressed() -> void:
 
 func _on_option_btn_pressed() -> void:
 	SceneManager.changeScene("res://scenes/ui-scenes/settings-screen/settings_screen.tscn")
+
+
+func _on_switch_acc_pressed() -> void:
+	SceneManager.changeScene("res://scenes/ui-scenes/login-screen-v2/login_screen.tscn")
