e46327428d29febd02f315b4ea9bde7cd8e7ab3b
chrono-04 <danielpenaflor04@gmail.com>
2024-09-05 22:28:37 +0800

    updated settings tab.gd
    added:
    center window function
    added center window function on
    window resolution selection

--- scenes/ui-scenes/settings-screen/settings_tab.gd
+++ scenes/ui-scenes/settings-screen/settings_tab.gd
@@ -7,16 +7,31 @@ func _on_res_option_item_selected(index: int) -> void:
 	match index:
 		0:
 			DisplayServer.window_set_size(Vector2i(1920, 1080))
+			center_window()
 		1:
 			DisplayServer.window_set_size(Vector2i(1366, 768))
+			center_window()
 		2:
 			DisplayServer.window_set_size(Vector2i(1280, 720))
+			center_window()
 
 
-# Window Mode Selection
+# Window Mode Selection	
 func _on_window_mode_item_selected(index: int) -> void:
 	match index:
 		0:
+			# changes the window to fullscreen mode
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 		1:
+			# changes the window to windowed mode
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			# puts the window to the center of the screen
+			center_window()
+
+
+# center window
+func center_window() -> void:
+	var screen_size = DisplayServer.screen_get_size()
+	var window_size = DisplayServer.window_get_size()
+	var centered_position = (screen_size - window_size) / 2
+	DisplayServer.window_set_position(centered_position)
---
+++ scenes/ui-scenes/title-screen/title_screen.gd
@@ -0,0 +1,7 @@
+extends Control
+
+
+
+
+func _on_option_btn_pressed() -> void:
+	SceneManager.changeScene("res://scenes/ui-scenes/settings-screen/settings_screen.tscn")
