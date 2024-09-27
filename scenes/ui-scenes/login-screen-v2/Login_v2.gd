d977937dfaa5de5c19286f6f6d7686200e1cd662
chrono-04 <danielpenaflor04@gmail.com>
2024-09-27 10:35:31 +0800

    Revert "deleted unrelated files"
    This reverts commit 63b000c4b9ba9dda253dc67e28c6d0416a18a2be.

---
+++ scenes/ui-scenes/login-screen-v2/Login_v2.gd
@@ -0,0 +1,32 @@
+extends Control
+
+
+func _ready():
+	pass
+	
+func resetLoginWindow():
+	%EmailInput.clear()
+	%PassInput.clear()
+
+func _on_signup_btn_pressed():
+	resetLoginWindow()
+	SceneManager.changeScene("res://scenes/ui-scenes/sign-up-screen/sign_up_screen.tscn")
+	
+
+func _on_login_btn_pressed():
+	# Validate the fields first
+	# 1. Make sure no fields are empty.
+	if(%EmailInput.text == "" || %PassInput.text == ""):
+		Notification.showMessage("Please fill up empty fields.", 10)
+	else:
+		var result = await ServerManager.loginWithEmailAndPassword(%EmailInput.text, %PassInput.text)
+		if result == OK:
+			Notification.showMessage("Login succesfully!", 3)
+			await get_tree().create_timer(3.0).timeout
+			# Change screen to title screen
+			SceneManager.changeScene("res://src/SceneManager/Scenes/Loading/Loading.tscn")
+		else:
+			Notification.showMessage("Login failed, please try again.", 3)
+
+func _on_close_btn_pressed():
+	get_tree().quit()
