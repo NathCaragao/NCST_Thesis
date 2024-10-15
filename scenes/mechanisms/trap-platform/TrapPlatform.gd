103edb029d38713c8931b515adcb6f5dc574d72c
chrono-04 <danielpenaflor04@gmail.com>
2024-10-15 15:19:04 +0800

    removed some unnecessary lines of code

--- scenes/mechanisms/trap-platform/TrapPlatform.gd
+++ scenes/mechanisms/trap-platform/TrapPlatform.gd
@@ -2,9 +2,9 @@ extends AnimatableBody2D
 @export var delay_before_moving: float = 1.0  # Time delay before the platform moves
 @export var animation_name: String = "move_platform"  # Name of the animation to play
 
-@onready var timer: Timer = $Timer
+#@onready var timer: Timer = $Timer
 @onready var trapzone = $trapzone
-@onready var detection_area: Area2D = $DetectionArea
+#@onready var detection_area: Area2D = $DetectionArea
 
 
 func _on_trap_body_entered(body):
