c99e5bafd8e3f41560432853e718023ae8e7455f
chrono-04 <danielpenaflor04@gmail.com>
2024-10-15 15:30:38 +0800

    added push function in player script

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -7,6 +7,15 @@ extends CharacterBody2D
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 @export var move_speed : float = 400
 
+@export var push = 40
+@export var SPEED: float = 200.0
+
+
+func _physics_process(delta: float) -> void:
+	push_objects()
+
+
+
 
 # flip sprite
 func flip_sprite() -> void:
@@ -18,3 +27,10 @@ func flip_sprite() -> void:
 		sprite.flip_h = true
 		# hitbox here
 		# skill hitbox here
+
+# function for pushing objects such as boxes
+func push_objects() -> void:
+	for i in get_slide_collision_count():
+		var c = get_slide_collision(i)
+		if c.get_collider() is RigidBody2D:
+			c.get_collider().apply_central_impulse(-c.get_normal() * (push + SPEED))
