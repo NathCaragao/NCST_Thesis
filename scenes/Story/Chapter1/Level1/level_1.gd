31bd0bd33afeea11dd63fa326e3d1713b65f9b1d
chrono-04 <danielpenaflor04@gmail.com>
2024-10-21 15:01:10 +0800

    added Interaction System

---
+++ scenes/Story/Chapter1/Level1/level_1.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@onready var interaction_area: InteractionArea = $InteractionArea
+
+func _ready() -> void:
+	interaction_area.interact = Callable(self, "on_interact")
+
+
+func on_interact() -> void:
+	print("SEEEEEEEEEGGGGGGGGGSSSSSSSSSSSS")
