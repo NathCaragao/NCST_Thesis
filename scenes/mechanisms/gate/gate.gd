96a9faee3f3d8f5af317ca1a651e022e7e129503
chrono-04 <danielpenaflor04@gmail.com>
2024-10-21 19:20:44 +0800

    added gate scene and script

---
+++ scenes/mechanisms/gate/gate.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+# references
+# animation player
+# related puzzle reference
+
+func _ready() -> void:
+	pass
+	
+
+# create a function that checks if the is_activated boolean is true
+# play gate opening animation
