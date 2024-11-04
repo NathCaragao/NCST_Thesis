1a9bb168075b218289f7470429f1a89f1a7b7ba4
chrono-04 <danielpenaflor04@gmail.com>
2024-11-04 15:16:20 +0800

    added cyclop enemy

---
+++ scenes/enemies/cyclop-boss/cyclop.gd
@@ -0,0 +1,34 @@
+class_name Megalus
+extends CharacterBody2D
+
+@onready var sprite: Sprite2D = $Sprite2D as Sprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer as AnimationPlayer
+@onready var hurt_box_shape: CollisionShape2D = $CollisionShape2D as CollisionShape2D
+
+# stats
+var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var move_speed : float = 400
+
+# flip sprite
+func flip_sprite() -> void:
+	if velocity.x > 0:
+		sprite.flip_h = false
+		$EnemyHealthComp/Hitbox/CollisionShape2D.position.x = 106
+		$EnemyHealthComp/Hurtbox/CollisionShape2D.position.x = 9
+	elif velocity.x < 0:
+		sprite.flip_h = true
+		$EnemyHealthComp/Hitbox/CollisionShape2D.position.x = -106
+		$EnemyHealthComp/Hurtbox/CollisionShape2D.position.x = -9
+
+# mapping animations for fsm
+var animation_mapping = {
+	"enemy-run" : "cyclop-run",
+	"enemy-attack" : "cyclop-attack",
+	"enemy-idle" : "cyclop-idle",
+	"enemy-hit" : "cyclop-hit",
+	"enemy-dead" : "cyclop-death"
+}
+
+func play_animation(animation_name: String) -> void:
+	if animation_name in animation_mapping:
+		animation_player.play(animation_mapping[animation_name])
