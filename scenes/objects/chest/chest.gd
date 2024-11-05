389babe4b4b517ee7046c47c37096a51371db37a
chrono-04 <danielpenaflor04@gmail.com>
2024-11-05 22:31:18 +0800

    added chest to level 1 scene

--- scenes/objects/chest/chest.gd
+++ scenes/objects/chest/chest.gd
@@ -1,6 +1,11 @@
 class_name Chest
 extends Node2D
 
+@onready var interaction_area: InteractionArea = $InteractionArea
+
+func _ready() -> void:
+	interaction_area.interact = Callable(self, "on_interact")
+
 func open() -> void:
 	$Closed.visible = false
 	$Opened.visible = true
@@ -8,3 +13,6 @@ func open() -> void:
 func close() -> void:
 	$Opened.visible = false
 	$Closed.visible = true
+
+func on_interact() -> void:
+	open()
