4f243aac9ca548ffeac6421267cfc43580fd6ce4
chrono-04 <danielpenaflor04@gmail.com>
2024-11-07 22:24:53 +0800

    replaced tree_bg sprite

--- scenes/Story/Chapter1/Level1/level_1.gd
+++ scenes/Story/Chapter1/Level1/level_1.gd
@@ -1,11 +1,5 @@
 extends Node2D
 
-@onready var interaction_area: InteractionArea = $InteractionArea
-
 
 func _ready() -> void:
-	interaction_area.interact = Callable(self, "on_interact")
-
-
-func on_interact() -> void:
-	print("SEEEEEEEEEGGGGGGGGGSSSSSSSSSSSS")
+	pass
