da67a1f05ebd2921957ec0fa9336dddfbbedc0be
chrono-04 <danielpenaflor04@gmail.com>
2024-11-08 09:18:26 +0800

    configured fail screen

--- scenes/Story/Chapter1/Level1/level_1.gd
+++ scenes/Story/Chapter1/Level1/level_1.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+@export var player : PlayerHercules
+@export var fail_screen: Control
 
 func _ready() -> void:
-	pass
+	player.connect("PlayerDead", Callable(self, "on_player_dead"))
+
+
+func on_player_dead() -> void:
+	fail_screen.visible = true
---
+++ scenes/ui-scenes/fail-screen/fail_screen.gd
@@ -0,0 +1,7 @@
+extends Control
+
+
+# restart btn
+func _on_restart_btn_pressed() -> void:
+	get_tree().reload_current_scene()
+	visible = false
