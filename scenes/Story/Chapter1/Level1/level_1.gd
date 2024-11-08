7fa779f474f1768c3fc8226fe28434b72b0e833e
chrono-04 <danielpenaflor04@gmail.com>
2024-11-08 10:02:23 +0800

    implemented pause_screen

--- scenes/Story/Chapter1/Level1/level_1.gd
+++ scenes/Story/Chapter1/Level1/level_1.gd
@@ -2,10 +2,19 @@ extends Node2D
 
 @export var player : PlayerHercules
 @export var fail_screen: Control
+@export var pause_screen : Control
+
+var paused : bool = false
 
 func _ready() -> void:
 	player.connect("PlayerFail", Callable(self, "on_player_fail"))
 
-
+# when player dies: fail screen opens
 func on_player_fail() -> void:
 	fail_screen.open()
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause_game"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		pause_screen.open()
+		get_tree().paused = true
