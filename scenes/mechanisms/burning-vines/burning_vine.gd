a37291f1851abe7bbd3bad0fbd1e78f172227f47
chrono-04 <danielpenaflor04@gmail.com>
2024-11-11 13:08:29 +0800

    setup burning vine node

---
+++ scenes/mechanisms/burning-vines/burning_vine.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var vine: Sprite2D = $Vine
+@onready var fire_1: Sprite2D = $Fire
+@onready var fire_2: Sprite2D = $Fire2
