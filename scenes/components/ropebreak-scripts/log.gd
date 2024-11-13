f895422b6d6fef82df36fb114c54a95e361e773f
chrono-04 <danielpenaflor04@gmail.com>
2024-11-13 12:58:52 +0800

    added static collision in log object scene

--- scenes/components/ropebreak-scripts/log.gd
+++ scenes/components/ropebreak-scripts/log.gd
@@ -1,6 +1,6 @@
 extends Area2D
 @onready var sprite_2d = $Sprite2D
-
+@export var static_collision : StaticBody2D
 
 func destroy_log() -> void:
 	# put destory log animation here
@@ -8,6 +8,7 @@ func destroy_log() -> void:
 	# queue free the whole log scene in the animation track
 	sprite_2d.queue_free()
 	$CollisionShape2D.queue_free()
+	$StaticBody2D/CollisionShape2D.queue_free()
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Debris"):
