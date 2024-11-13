e5de5096e39e07a0c3c9eba44c76411e66d4b8c6
chrono-04 <danielpenaflor04@gmail.com>
2024-11-13 21:56:15 +0800

    added dialogs 3 to 6

--- scenes/components/dialog-activator/dialog_activator.gd
+++ scenes/components/dialog-activator/dialog_activator.gd
@@ -3,7 +3,13 @@ extends Area2D
 
 # enter the dialog file name you want to play
 @export var dialog_file : String = ""
+
 var dialog_played : bool = false
+
+# setting the collision mask to interact with the player
+func _ready() -> void:
+	collision_layer = 0
+	collision_mask = 8
 
 func play_dialog() -> void:
 	if not dialog_played:
