ff47b4622c969f130ba81179f9c60b7a9b56870b
chrono-04 <danielpenaflor04@gmail.com>
2024-11-16 22:39:34 +0800

    added spartan enemy

--- fsm/enemy-states/enemy-idle.gd
+++ fsm/enemy-states/enemy-idle.gd
@@ -3,9 +3,11 @@ extends State
 
 # references and variables
 @export var actor : CharacterBody2D
+var player : PlayerHercules
+var direction
 
 func enter() -> void:
-	pass
+	actor.play_animation("enemy-idle")
 
 func update(delta: float) -> void:
 	pass
@@ -13,10 +15,22 @@ func update(delta: float) -> void:
 func physics_update(delta: float) -> void:
 	# putting the gravity to the actor
 	actor.velocity.y += actor.gravity * delta
+	actor.move_and_slide()
+	
+	if is_instance_valid(player) and is_instance_valid(actor):
+		direction = player.global_position - actor.global_position
+		
+		# Set the y-component to 0 to restrict movement to the x-axis (horizontal only)
+		direction.y = 0
 	
 	if actor.velocity.x == 0:
 		actor.play_animation("enemy-idle")
 	
+	actor.flip_sprite()
+	
 	# transitions to other states
+	if direction.length() < 90:
+		print("ENEMY is following the player")
+		Transitioned.emit(self, "enemyfollow")
 	
 	# transitions to enemy-follow
