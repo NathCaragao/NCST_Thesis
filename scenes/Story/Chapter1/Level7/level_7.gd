8eb0e0ecb1588b57ebed7d4d2f22ba76177a127b
chrono-04 <danielpenaflor04@gmail.com>
2024-11-17 00:29:11 +0800

    lvl 7: added enemy spawn code

---
+++ scenes/Story/Chapter1/Level7/level_7.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+@export var enemy_scenes : Array[PackedScene] = []
+@export var player : PlayerHercules
+@export var fail_screen: Control
+@export var pause_screen : Control
+@export var victory_screen : Control
+@onready var marker_2d: Marker2D = $SpawnArea/Marker2D
+
+var paused : bool = false
+
+func _ready() -> void:
+	player.connect("PlayerFail", Callable(self, "on_player_fail"))
+
+# when player dies: fail screen opens
+func on_player_fail() -> void:
+	fail_screen.open()
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause_game"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		pause_screen.open()
+		get_tree().paused = true
+
+
+func spawn_enemy(index : int) -> void:
+	if enemy_scenes.is_empty():
+		return
+	
+	var enemy = enemy_scenes[index].instantiate()
+	enemy.global_position = marker_2d.global_position
+	#get_parent().add_child(enemy)
+	call_deferred("add_child", enemy)
+	print("Enemy spawned")
+
+
+func _on_spawn_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		spawn_enemy(0)
