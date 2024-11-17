caf79032b9de4b961cecf0151d2947bb88b7d255
kuyajobert <limosnero.nicoangelo@ncst.edu.ph>
2024-11-17 14:38:41 +0800

    level_10

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -5,21 +5,19 @@ extends CharacterBody2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @export var push = 40
 @export var SPEED: float = 200.0
-
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 @export var move_speed : float = 400
-
 func _ready() -> void:
-	pass
+	# Initialize the state machine, passing a reference of the player to the states,
+	# that way they can move and react accordingly
+
 	
 	
 	for i in get_slide_collision_count():
 		var c = get_slide_collision(i)
 		if c.get_collider() is RigidBody2D:
 			c.get_collider().apply_central_impulse(-c.get_normal() * (push + SPEED))
-	
 
-# flip sprite
 func flip_sprite() -> void:
 	if velocity.x > 0:
 		sprite.flip_h = false
