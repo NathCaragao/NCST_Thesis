e9f4becf1bfbb653208e00982e1f7bb672594585
chrono-04 <danielpenaflor04@gmail.com>
2024-11-18 00:33:11 +0800

    added Orthrus enemy

---
+++ scenes/enemies/orthrus/orthrus.gd
@@ -0,0 +1,31 @@
+class_name EnemyOrthrus
+extends CharacterBody2D
+
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var enemy_health_comp: EnemyHealthComp = $EnemyHealthComp
+
+var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var move_speed : float = 400
+
+# mapping animations for fsm
+var animation_mapping = {
+	"enemy-run" : "orthrus-run",
+	"enemy-attack" : "orthrus-attack",
+	"enemy-idle" : "orthrus-idle",
+	"enemy-hit" : "orthrus-hit",
+	"enemy-dead" : "orthrus-death"
+}
+
+func play_animation(animation_name: String) -> void:
+	if animation_name in animation_mapping:
+		animation_player.play(animation_mapping[animation_name])
+
+# flip sprite
+func flip_sprite() -> void:
+	if velocity.x < 0:
+		sprite.flip_h = false
+		$EnemyHealthComp/Hitbox.scale.x = 1
+	elif velocity.x > 0:
+		sprite.flip_h = true
+		$EnemyHealthComp/Hitbox.scale.x = -1
