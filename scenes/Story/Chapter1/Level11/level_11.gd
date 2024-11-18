5b2601726da15a6feb1ac6d811627c31f06ae47a
chrono-04 <danielpenaflor04@gmail.com>
2024-11-18 09:50:00 +0800

    add teleport mechanism

--- scenes/Story/Chapter1/Level11/level_11.gd
+++ scenes/Story/Chapter1/Level11/level_11.gd
@@ -3,7 +3,7 @@ extends Node2D
 @export var fail_screen: Control
 @export var pause_screen : Control
 @export var victory_screen : Control
-
+@export var actor : PlayerHercules
 var paused : bool = false
 
 func _ready() -> void:
@@ -18,3 +18,16 @@ func _process(delta: float) -> void:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		pause_screen.open()
 		get_tree().paused = true
+
+# teleport activation
+func _on_teleport_body_entered(body: Node2D) -> void:
+	call_deferred("player_teleport")
+
+func player_teleport() -> void:
+	# in-game screen fade out transition
+	LevelScreenTransition.transition()
+	await LevelScreenTransition.on_transition_finished
+	
+	# teleport the player to the specified position
+	actor.position.x = 2635
+	actor.position.y = 4685
