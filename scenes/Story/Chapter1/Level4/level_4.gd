1ef644ead7a1cf42af0b7acbd4b5e258fb93bff0
chrono-04 <danielpenaflor04@gmail.com>
2024-11-19 21:25:46 +0800

    level4: implemented dialogs

--- scenes/Story/Chapter1/Level4/level_4.gd
+++ scenes/Story/Chapter1/Level4/level_4.gd
@@ -4,11 +4,13 @@ extends Node2D
 @export var fail_screen: Control
 @export var pause_screen : Control
 @export var victory_screen : Control
+@export var boss_boar : CharacterBody2D
 
 var paused : bool = false
 
 func _ready() -> void:
 	player.connect("PlayerFail", Callable(self, "on_player_fail"))
+	boss_boar.connect("BossBoarDefeated", Callable(self, "on_defeat"))
 
 # when player dies: fail screen opens
 func on_player_fail() -> void:
@@ -19,3 +21,16 @@ func _process(delta: float) -> void:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		pause_screen.open()
 		get_tree().paused = true
+
+func on_defeat() -> void:
+	after_battle_dialog()
+
+func after_battle_dialog() -> void:
+	Dialogic.start("S4_11")
+	await Dialogic.timeline_ended # waits for the dialog to finish
+	
+	level_complete()
+
+func level_complete() -> void:
+	victory_screen.visible = true
+	victory_screen.update_scores()
