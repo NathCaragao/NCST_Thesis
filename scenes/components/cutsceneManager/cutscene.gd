1bbaa5b6a8a40c2c88802a9e1f973e5b8b356770
chrono-04 <danielpenaflor04@gmail.com>
2024-11-20 20:40:59 +0800

    level3 cutscene_opening
    added smooth transition
    change method to SceneManager

--- scenes/components/cutsceneManager/cutscene.gd
+++ scenes/components/cutsceneManager/cutscene.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 @export var dialog_file: String = ""
 @export var playlevel: String = ""  # Path to the next scene
@@ -8,10 +8,16 @@ func _ready():
 	if dialog_file != "":
 		Dialogic.start(dialog_file)
 		Dialogic.timeline_ended.connect(change_scene)
+	else:
+		print("Error: Empty Dialog file!")
 	
 func change_scene():
 	# Change to the scene specified in 'playlevel'
 	if playlevel != "":
-		get_tree().change_scene_to_file(playlevel)
+		# play screen transtion
+		LevelScreenTransition.transition()
+		await LevelScreenTransition.on_transition_finished # waits for the transition to end
+		
+		SceneManager.changeScene(playlevel)
 	else:
 		print("Error: 'playlevel' is not set!")
