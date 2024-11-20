ec2a2b7aebd77c4ff4422b91716447722cc08969
kuyajobert <limosnero.nicoangelo@ncst.edu.ph>
2024-11-20 22:53:48 +0800

    cutscenemanager code update

--- scenes/components/cutsceneManager/cutscene.gd
+++ scenes/components/cutsceneManager/cutscene.gd
@@ -1,23 +1,52 @@
 extends Control
 
-@export var dialog_file: String = ""
-@export var playlevel: String = ""  # Path to the next scene
+@export var dialog_file: String = ""  # Path to the dialog file
+@export var playlevel: String = ""   # Path to the next scene
+@export var target_node: Node = null  # The node to make invisible
+
+var dialog_instance  # To keep track of the Dialogic instance
 
 func _ready():
 	# Start the Dialogic dialog
+	target_node.visible = true
 	if dialog_file != "":
-		Dialogic.start(dialog_file)
-		Dialogic.timeline_ended.connect(change_scene)
+		dialog_instance = Dialogic.start(dialog_file)
+		add_child(dialog_instance)
+		dialog_instance.connect("dialogic_signal", Callable(self, "_on_dialogic_signal"))
+		Dialogic.signal_event.connect(_on_dialogic_signal)
 	else:
 		print("Error: Empty Dialog file!")
-	
+
+func _on_dialogic_signal(event: String):
+	print("event")
+	if event == "end":
+		change_scene()
+
 func change_scene():
+
 	# Change to the scene specified in 'playlevel'
 	if playlevel != "":
-		# play screen transtion
+		# Play screen transition
 		LevelScreenTransition.transition()
-		await LevelScreenTransition.on_transition_finished # waits for the transition to end
+		await LevelScreenTransition.on_transition_finished  # Wait for the transition to end
 		
+		
+		# Cleanup Dialogic instance
+		if dialog_instance:
+			print("cleanup")
+			dialog_instance = null
+			
+			
+		# Make the target node invisible	
+		if target_node:
+			if target_node.has_method("set_visible"):
+				target_node.visible = false
+				print("Target node has been made invisible.")
+			else:
+				print("Error: Target node does not support visibility!")
+		else:
+			print("Error: 'target_node' is not set!")
 		SceneManager.changeScene(playlevel)
+		
 	else:
 		print("Error: 'playlevel' is not set!")
