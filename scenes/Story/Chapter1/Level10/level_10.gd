1d3284cf764ca9fd5969bc3bb051eddc467f4df9
chrono-04 <danielpenaflor04@gmail.com>
2024-11-23 11:17:55 +0800

    level10:
    - added post dialog and victory screen

---
+++ scenes/Story/Chapter1/Level10/level_10.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+
+@export var fail_screen: Control
+@export var pause_screen : Control
+@export var victory_screen : Control
+
+func _ready() -> void:
+	Dialogic.signal_event.connect(on_level_complete)
+
+# when player dies: fail screen opens
+func on_player_fail() -> void:
+	fail_screen.open()
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause_game"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		pause_screen.open()
+		get_tree().paused = true
+
+func on_level_complete(argument : String) -> void:
+	if argument == "11labordone":
+		level_complete_screen()
+
+func level_complete_screen() -> void:
+	await get_tree().create_timer(2).timeout
+	
+	victory_screen.visible = true
+	victory_screen.update_scores()
