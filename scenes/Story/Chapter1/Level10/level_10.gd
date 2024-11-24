ec7b1b80d457937ad8c74574e35177e26d4bac53
kuyajobert <limosnero.nicoangelo@ncst.edu.ph>
2024-11-24 15:25:13 +0800

    level 10 update
    opening cutscene added
    modify ung part sa cave na may tumataas at bumababa na platform, nilagyan ko ng deathsone, pati spike ba tiles

--- scenes/Story/Chapter1/Level10/level_10.gd
+++ scenes/Story/Chapter1/Level10/level_10.gd
@@ -1,13 +1,15 @@
 extends Node2D
-
+@onready var cutscene_layer: CanvasLayer = $CanvasLayer
+var scene_path : String = "res://scenes/cutscenes-collection/level 10/level_10_opening.tscn"
 
 @export var fail_screen: Control
 @export var pause_screen : Control
 @export var victory_screen : Control
 
 func _ready() -> void:
+	CutsceneManager.set_canvas_layer(cutscene_layer)
 	Dialogic.signal_event.connect(on_level_complete)
-
+	opening_cutscene()
 # when player dies: fail screen opens
 func on_player_fail() -> void:
 	fail_screen.open()
@@ -27,3 +29,7 @@ func level_complete_screen() -> void:
 	
 	victory_screen.visible = true
 	victory_screen.update_scores()
+func opening_cutscene() -> void:
+	CutsceneManager.add_cutscene(scene_path, "opening6")
+	CutsceneManager.play_cutscene("opening6")
+	
