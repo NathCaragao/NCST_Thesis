867f825c25fa57b67c1c323eab67cea6a38d6bb0
kuyajobert <limosnero.nicoangelo@ncst.edu.ph>
2024-11-26 18:50:46 +0800

    level5.1 update
    victory screen
    fail screen
    paused screen

--- scenes/Story/Chapter1/Level5/level_5.1.gd
+++ scenes/Story/Chapter1/Level5/level_5.1.gd
@@ -4,12 +4,27 @@ var scene_path_1 : String = "res://scenes/cutscenes-collection/level 5-1/level_5
 var scene_path_2 : String = "res://scenes/cutscenes-collection/level 5-1/level_5_opening_2.tscn"
 @onready var canvas_layer: CanvasLayer = $CanvasLayer
 
+@export var player : PlayerHercules
+@export var fail_screen: Control
+@export var pause_screen : Control
+@export var victory_screen : Control
+
 func _ready() -> void:
 	# set canvas layer for cutscenes to be added
 	CutsceneManager.set_canvas_layer(canvas_layer)
 	Dialogic.signal_event.connect(on_op1) # opening 1 dialog signal ending
+	Dialogic.signal_event.connect(on_dialog_done) # dialogic signal
 	
 	opening_1()
+	
+# victory after dialog 3
+func on_dialog_done(argument: String) -> void:
+	if argument == "level5complete":
+		on_finish()
+
+func on_finish() -> void:
+	victory_screen.visible = true
+	victory_screen.update_scores()
 
 # cutscenes
 func opening_1() -> void:
