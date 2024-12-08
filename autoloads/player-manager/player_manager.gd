24668344d630cc13192600f3b0772350f5713600
chrono-04 <danielpenaflor04@gmail.com>
2024-12-08 19:57:15 +0800

    implemented player stat upgrade shop

--- autoloads/player-manager/player_manager.gd
+++ autoloads/player-manager/player_manager.gd
@@ -9,4 +9,4 @@ var player_weapon_dmg : float = 3.0
 var player_attack : float = 10.0
 var player_defense : float = 10.0
 var player_health : float = 100.0
-var coins : int = 100.0
+var coins : int = 100
--- scenes/ui-scenes/shop-screen(final)/stat-window/stat_upgrade_panel.gd
+++ scenes/ui-scenes/shop-screen(final)/stat-window/stat_upgrade_panel.gd
@@ -40,25 +40,25 @@ extends Control
 @export_category("Attack Cost")
 @export var atk_upgrade : float = 10.0
 @export var atk_cost : int = 10
-@export var atk_level : int = 1
+@export var atk_max_level : int = 5
 
 
 # health stat information
 @export_category("Health Cost")
 @export var hp_upgrade : float = 20.0
 @export var hp_cost : int = 10
-@export var hp_level : int = 1
+@export var hp_max_level : int = 5
 
 # defense stat information
 @export_category("Defense Cost")
 @export var def_upgrade : float = 10.0
 @export var def_cost : int = 10
-@export var def_level : int = 1
+@export var def_max_level : int = 5
 
 # speed stat information
 @export var speed_upgrade : float = 5.0
 @export var speed_cost : int = 10
-@export var speed_level : int = 1
+@export var speed_max_level : int = 5
 
 
 # ITEM 1
@@ -100,6 +100,74 @@ func item4_exited() -> void:
 	frame_hover_4.visible = false
 	clear_info_display()
 
+# upgrading the attack stat
+func upgrade_attack() -> void:
+	# check if users currency suffice
+	if PlayerManager.coins >= atk_cost:
+		if $ItemFrame1/AtkLevel.value < atk_max_level:
+			# subtract atk upgrade cost from player coins
+			PlayerManager.coins -= atk_cost
+			# increase the attack
+			PlayerManager.player_attack += atk_upgrade
+			# update UI
+			$ItemFrame1/AtkLevel.value += 1
+		elif $ItemFrame1/AtkLevel.value == atk_max_level:
+			print("Already Reached the max level!")
+		
+		print("Current attack:", str(PlayerManager.player_attack))
+	elif PlayerManager.coins < atk_cost:
+		print("You don't have enough coins")
+
+# upgrading the health stat
+func upgrade_health() -> void:
+	# check if users currency suffice
+	if PlayerManager.coins >= hp_cost:
+		if $ItemFrame2/HpLevel.value < hp_max_level:
+			# subtract hp upgrade cost from player coins
+			PlayerManager.coins -= hp_cost
+			# increase the health
+			PlayerManager.player_health += hp_upgrade
+			# update UI
+			$ItemFrame2/HpLevel.value += 1
+		elif $ItemFrame2/HpLevel.value == hp_max_level:
+			print("Already reached the max level!")
+	
+	elif PlayerManager.coins < hp_cost:
+		print("You don't have enough coins")
+
+# upgrading the defense stat
+func upgrade_defense() -> void:
+	# check if users currency suffice
+	if PlayerManager.coins >= def_cost:
+		if $ItemFrame3/DefLevel.value < def_max_level:
+			# subtract hp upgrade cost from player coins
+			PlayerManager.coins -= def_cost
+			# increase defense stat value
+			PlayerManager.player_defense += def_upgrade
+			# update the UI
+			$ItemFrame3/DefLevel.value += 1
+		elif $ItemFrame3/DefLevel.value == def_max_level:
+			print("Already reached the max level!")
+	
+	elif PlayerManager.coins < def_cost:
+		print("You don't have enough coins")
+
+func upgrade_speed() -> void:
+	# check if users currency suffice
+	if PlayerManager.coins >= speed_cost:
+		if $ItemFrame4/SpeedLevel.value < speed_max_level:
+			# subtract hp upgrade cost from player coins
+			PlayerManager.coins -= speed_cost
+			# increase the speed stat
+			PlayerManager.player_move_speed += speed_upgrade
+			# update the UI
+			$ItemFrame4/SpeedLevel.value += 1
+		elif $ItemFrame4/SpeedLevel.value == speed_max_level:
+			print("Already reached the max level!")
+	
+	elif PlayerManager.coins < speed_cost:
+		print("You don't have enough coins")
+
 func display_item_info(item_name: String, item_effect: String, item_desc: String) -> void:
 	statname.text = item_name
 	stat_effect.text = item_effect
@@ -109,3 +177,19 @@ func clear_info_display() -> void:
 	statname.text = ""
 	stat_effect.text = ""
 	stat_desc.text = ""
+
+
+func _on_atk_upgrade_pressed() -> void:
+	upgrade_attack()
+
+
+func _on_hp_upgrade_pressed() -> void:
+	upgrade_health()
+
+
+func _on_def_upgrade_pressed() -> void:
+	upgrade_defense()
+
+
+func _on_speed_upgrade_pressed() -> void:
+	upgrade_speed()
