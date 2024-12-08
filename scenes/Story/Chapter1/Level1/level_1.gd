00c01e0841e7cb97dafd9266a190101e216120a0
NathCaragao <166087791+NathCaragao@users.noreply.github.com>
2024-12-09 00:02:15 +0800

    Solved Pause error during dialogs
    -- Added to Level 1, summary
    * add new boolean "isDialogPlaying" and set it to false
    
    * connect to Dialogic.timeline_started and Dialogic.timeline_ended, the funcs should set isDialogPlaying to true and false, respectively

--- scenes/Story/Chapter1/Level1/level_1.gd
+++ scenes/Story/Chapter1/Level1/level_1.gd
@@ -12,6 +12,8 @@ var scene_path : String = "res://scenes/cutscenes-collection/level 1/level_1_ope
 @export var nemean_lion : CharacterBody2D
 
 var paused : bool = false
+var isDialogPlaying = false
+		
 
 func _ready() -> void:
 	# resets the score and player inventory for a clean state
@@ -29,12 +31,26 @@ func _ready() -> void:
 	
 	# Connect Dialogic "end" signal to handle audio playback
 	Dialogic.signal_event.connect(on_dialogic_signal_play_bgm)
+	Dialogic.timeline_started.connect(on_dialog_start)
+	Dialogic.timeline_ended.connect(on_dialog_end)
 	
 	# play opening cutscene
+	#isDialogPlaying = true
 	opening_cutscene()
+
+# Handles the start signal from Dialogic
+func on_dialog_start():
+	isDialogPlaying = true
+	print_debug("Started dialog, isDialogPlaying: %s" % str(isDialogPlaying))
+
+func on_dialog_end():
+	isDialogPlaying = false
+	print_debug("Ended dialog, isDialogPlaying: %s" % str(isDialogPlaying))
 
 # Handles the "end" signal from Dialogic
 func on_dialogic_signal_play_bgm(event: String) -> void:
+	isDialogPlaying = false
+	print_debug("Ended dialog, isDialogPlaying: %s" % str(isDialogPlaying))
 	if event == "end":
 		# Play the lively audio
 		if lively:
@@ -47,7 +63,7 @@ func on_player_fail() -> void:
 	fail_screen.open()
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("pause_game") and !get_tree().paused:
+	if Input.is_action_just_pressed("pause_game") and !get_tree().paused and !isDialogPlaying:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		pause_screen.open()
 		get_tree().paused = true
@@ -55,6 +71,7 @@ func _process(delta: float) -> void:
 
 
 func on_lion_defeated() -> void:
+	#isDialogPlaying = true
 	level_cleared()
 	# Await sending rewards update to server
 	var freeCurrencyCollectedThisLevel = ScoreManager.collected_items["coin"]
@@ -66,6 +83,7 @@ func on_lion_defeated() -> void:
 		elif i == 3:
 			Notification.showMessage("Failed to save rewards to Server. Please restart the game", 5.0)
 	# reset score manager for future use
+	#isDialogPlaying = false
 	ScoreManager.reset_score()
 	
 	
@@ -74,6 +92,7 @@ func on_lion_defeated() -> void:
 func opening_cutscene() -> void:
 	CutsceneManager.add_cutscene(scene_path, "opening1")
 	CutsceneManager.play_cutscene("opening1")
+	#isDialogPlaying = false
 
 func level_cleared() -> void:
 	victory_screen.visible = true # shows the victory screen
