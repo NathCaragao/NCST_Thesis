3c6849c9d1a617d45df6820ebbc328cbca187ad8
chrono-04 <danielpenaflor04@gmail.com>
2024-12-13 07:10:49 +0800

    Atalanta Restructure I
    - added run state
    - added idle state

--- scenes/atalanta-player/atalanta.gd
+++ scenes/atalanta-player/atalanta.gd
@@ -1,7 +1,33 @@
+class_name PlayerAtalanta
 extends CharacterBody2D
 
+
+
+# FINAL VARIABLES
+# -- Updated and used for server-client comm
+# THIS IS ALREADY SETUP FOR DEFAULT VALUES
+var playerGameData = {
+	"playerId" = "",
+	"displayName" = "",
+	"isControlled" = true,
+	"isJumping" = false,
+	"isAttacking" = false,
+	"isSkill" = false,
+	"direction" = 0,
+	"weaponMode" = "Melee",
+	"velocity" = Vector2(0, 0),
+	"position" = Vector2(0, 0),
+}
+
+# -- One time setup
+var move_speed: float
+var defense : float = 5.0
+var base_dmg : float = 10.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
 
 
 # flip sprite
