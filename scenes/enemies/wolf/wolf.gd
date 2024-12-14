0b8ec5303309d6f46b2527c29ba70a258702c13b
chrono-04 <danielpenaflor04@gmail.com>
2024-12-15 02:45:49 +0800

    added battle tutorial screen

--- scenes/enemies/wolf/wolf.gd
+++ scenes/enemies/wolf/wolf.gd
@@ -56,4 +56,5 @@ func on_dead_quota() -> void:
 	emit_signal("QuestQuota")
 
 func wolf_loot() -> void:
-	LootDropSystem.drop_multiple_items(global_position, ["gear"], randi_range(1, 8))
+	pass
+	#LootDropSystem.drop_multiple_items(global_position, ["gear"], randi_range(1, 8))
